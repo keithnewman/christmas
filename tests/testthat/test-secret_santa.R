36d0d27a27a247136b19b7dd3cad857280ba576d
Keith Newman <keithnewman@users.noreply.github.com>
2024-11-12 18:22:41 +0000

    Improve test thoroughness

--- tests/testthat/test-secret_santa.R
+++ tests/testthat/test-secret_santa.R
@@ -1,6 +1,10 @@
-test_that("secret_santa() won't assign a person to themself", {
-  naughty_list <- secret_santa(letters)
-  expect_false(any(naughty_list$gift_giver == naughty_list$gift_recipient))
+test_that("secret_santa() won't assign a person to themself and everyone gives and receives gift", {
+  for (chestnut in sample.int(10000, 300)) {
+    expect_no_error(naughty_list <- secret_santa(letters, seed = !!chestnut))
+    expect_true(all(naughty_list$gift_giver != naughty_list$gift_recipient))
+    expect_true(all(letters %in% naughty_list$gift_giver))
+    expect_true(all(letters %in% naughty_list$gift_recipient))
+  }
 })
 
 test_that("secret_santa() warns if the same person appears twice in the list", {
