c75fdaf524ded3e92ce8e830396fe41892c3d1cc
Keith Newman <keithnewman@users.noreply.github.com>
2024-11-12 17:44:16 +0000

    Don't modify user's session seed

--- R/secret_santa.R
+++ R/secret_santa.R
@@ -11,13 +11,17 @@ secret_santa <- function(names, seed = 2512) {
   if (length(unique(names)) != length(names)) {
     stop("Non-unique names provided - do you need to add a surname?")
   }
-  set.seed(seed)
-  random_names <- sample(names)
-  output <- data.frame(
-    gift_giver = random_names,
-    gift_recipient = random_names[c(2:length(random_names), 1)]
+  withr::with_seed(
+    seed = seed,
+    code = {
+      random_names <- sample(names)
+      output <- data.frame(
+        gift_giver = random_names,
+        gift_recipient = random_names[c(2:length(random_names), 1)]
+      )
+      output <- output[sample(seq_len(nrow(output))), ]
+      rownames(output) <- seq_len(nrow(output))
+      return(output)
+    }
   )
-  output <- output[sample(seq_len(nrow(output))), ]
-  rownames(output) <- seq_len(nrow(output))
-  return(output)
 }
