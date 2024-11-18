8d6bbd711332636720901d3ad5ed47f0847d33c5
jcken95 <jack_kennedy1@hotmail.co.uk>
2024-11-18 14:09:26 +0000

    fix: prevent non-secret games

--- R/secret_santa.R
+++ R/secret_santa.R
@@ -13,6 +13,11 @@ secret_santa <- function(names, seed = 2512) {
   if (anyDuplicated(names)) {
     stop("Non-unique names provided - do you need to add a surname?")
   }
+
+  if(length(names) <= 2) {
+    stop("It's not a secret with 2 or fewer players")
+  }
+
   withr::with_seed(
     seed = seed,
     code = {
